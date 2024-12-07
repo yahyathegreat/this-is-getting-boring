60a57b0a2f3064cfd3eff129b4c004f9f9ccc264
yahyathegreat <yahyamardan1234@gmail.com>
2024-12-07 12:07:57 +0500

    get

--- main.py
+++ main.py
@@ -19,3 +19,14 @@ for key,value in student_data.items():
     if value not in result.values():
         result[key] = value
         print(result)
+test_dict = {'codingal'  : 2, 'is' : 2, 'best' : 2, 'for' : 2, 'coding : 1'}
+print("the oringinal dictionary :" + str(test_dict))
+K = 2
+res = 0
+for key in test_dict:
+    if test_dict[key] == K:
+        res = res + 1
+print("frequency of K is :" + str(res))
+SPCMcountry_code = {'India' : '0091'}
+print("country code for India -")
+print("country_code.get('India), 'Not found")
